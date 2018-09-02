0564ce59448af8aae82315b0b3da80f737992570
sandeepvrm029 <sandeepvrm029@gmail.com>
2018-09-02 13:02:09 +0530

    Refactored.

--- colorGame.js
+++ colorGame.js
@@ -1,74 +1,42 @@
-var numSquares = 6;
-var colors = generateRandomColors(numSquares);
 var squares = document.getElementsByClassName("square");
 var color_picked = document.getElementById("color_code");
 var message = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var hardMode = document.querySelector("#hardBtn");
-var easyMode = document.querySelector("#easyBtn");
-var pickedColor = pickColor();
+var modeButtons = document.querySelectorAll(".modes");
+var numSquares = 3;
+var colors = [];
+var pickedColor;
 
-color_picked.textContent = pickedColor;
+newColors();
 
-easyMode.addEventListener("click", function() {
-    numSquares = 3;
-    hardMode.classList.remove("selected");
-    easyMode.classList.add("selected");
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    color_picked.textContent = pickedColor;
-    h1.style.backgroundColor = "steelblue";
-    for(var i = 0; i < squares.length; i++) {
-        if( colors[i] ) {
-            squares[i].style.backgroundColor = colors[i];
+for(var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+        for(var j = 0; j < modeButtons.length; j++) { 
+            modeButtons[j].classList.remove("selected");
         }
-        else {
-            squares[i].style.display = "none";
-        }
-    }
-})
-
-hardMode.addEventListener("click", function() {
-    numSquares = 6;
-    easyMode.classList.remove("selected");
-    hardMode.classList.add("selected");
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    color_picked.textContent = pickedColor;
-    h1.style.backgroundColor = "steelblue";
-    for(var i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";       
-    }
-})
+        this.classList.add("selected");
+        (this.textContent === "Easy") ? numSquares = 3 : numSquares = 6;
+        newColors();
+    })
+}
 
 resetButton.addEventListener("click", function() {
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    color_picked.textContent = pickedColor;
-    for(var i = 0; i < colors.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-    }
-    resetButton.textContent = "New colors";
-    h1.style.backgroundColor = "steelblue";
-    message.textContent = "Pick a color.";
+    newColors();
 })
 
 for(var i = 0; i < colors.length; i++) {
     squares[i].style.backgroundColor = colors[i];
-
     squares[i].addEventListener("click", function() {
         var clickedColor = this.style.backgroundColor;
-
         if(clickedColor === pickedColor) {
-            message.textContent = "Correct color.";
+            message.textContent = "Correct color";
             changeColor(clickedColor);
             h1.style.background = clickedColor;
             resetButton.textContent = "Play again?";
         } else {
             this.style.backgroundColor = "#232323";
-            message.textContent = "Wrong color. Try again.";
+            message.textContent = "Try again";
         }
     })
 }
@@ -95,6 +63,22 @@ function randomColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
-
     return ("rgb(" + r + ", " + g + ", " + b + ")");
+}
+
+function newColors() {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    color_picked.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    resetButton.textContent = "New colors";
+    h1.style.backgroundColor = "steelblue";
+    message.textContent = "Pick a color";
 }
